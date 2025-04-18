5f736431b08506009940368b2a1d8205c4b81dde
kwangmin.kim <kwangmin.kim@lgepartner.com>
2025-04-18 14:05:13 +0900

    chore: add telescope extension - livegrep args

--- lua/plugins/configs/telescope.lua
+++ lua/plugins/configs/telescope.lua
@@ -1,4 +1,5 @@
+local telescope = require('telescope')
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<F6>', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<F3>', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<F3>', ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", { desc = 'Telescope live grep' })
