addea5ed6fa0278abf6e5d7d6b53813c49c937b9
kwangmin.kim <kwangmin.kim@lgepartner.com>
2025-04-25 17:03:19 +0900

    chore: add parameter for live_grep plugin - telescope

--- lua/plugins/configs/telescope.lua
+++ lua/plugins/configs/telescope.lua
@@ -1,5 +1,27 @@
 local telescope = require('telescope')
 local builtin = require('telescope.builtin')
+local lga_actions = require("telescope-live-grep-args.actions")
+
+telescope.setup {
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-u>"] = false,
+        ["<C-d>"] = false,
+      },
+    },
+  },
+  extensions = {
+    live_grep_args = {
+      auto_quoting = true,
+      mappings = {
+        i = {
+          ["<C-f>"] = lga_actions.quote_prompt({ postfix = " -g '!tags' -g '!build-*/' -g '!tests/' -g '!unittests/' -g '!*.cmake' -g '!*.vsbl'" }),
+        },
+      },
+    },
+  },
+}
 
 vim.keymap.set('n', '<F6>', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<F3>', ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", { desc = 'Telescope live grep' })
